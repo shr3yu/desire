ab3dd69bbe5b0ebba2822e4423795446c40c53ed
Shreya pawar mamidi <mamidishreyapawar@gmail.com>
2024-06-22 01:56:09 -0400

    change server

--- frontend/desire-app/src/utils/axiosInstancs.js
+++ frontend/desire-app/src/utils/axiosInstancs.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./constants";
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: process.env.REACT_APP_API_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
